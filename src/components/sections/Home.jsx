d540e2e56f427a84d55b00d1b6c6ab3487f45f7e
Ramdeniakki <ramdeniakki35@gmail.com>
2025-02-19 21:05:51 +0530

    Added smooth Scrolling Behavior

--- src/components/sections/Home.jsx
+++ src/components/sections/Home.jsx
@@ -43,7 +43,7 @@ export const Home = () => {
               </a>
 
               <a
-                href="src\components\sections\File\resume.pdf"
+                href="public\File\resume.pdf"
                 target="_blank"
                 rel="noopener noreferrer"
                 className="border-2 border-cyan-500 text-cyan-500 py-3 px-6 rounded-full font-medium transition-all duration-300 transform hover:scale-105 hover:bg-cyan-500 hover:text-white hover:shadow-xl"
